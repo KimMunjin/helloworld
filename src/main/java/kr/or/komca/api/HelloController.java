58b2e0098c73ff3a7ee1c3ecbfa605d763925ab5
Kim Munjin <reminisce057@gmail.com>
2024-12-16 16:45:03 +0900

    배포 관련 설정

--- src/main/java/kr/or/komca/api/HelloController.java
+++ src/main/java/kr/or/komca/api/HelloController.java
@@ -2,6 +2,7 @@ package kr.or.komca.api;
 
 import kr.or.komca.dto.HelloResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/public/api")
 @RequiredArgsConstructor
@@ -16,6 +18,8 @@ public class HelloController {
 
     @GetMapping("/hello")
     public ResponseEntity<HelloResponse> hello() {
+        log.debug("-------------------- hello" + LocalDateTime.now());
+        System.out.println("----------------- hello!!" + LocalDateTime.now());
         HelloResponse response = new HelloResponse(
                 "안녕하세요! Spring Boot API 입니다../",
                 LocalDateTime.now()
